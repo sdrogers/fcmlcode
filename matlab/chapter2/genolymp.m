2018a1ad97f7c3624bfef6b70638313a4b356bb6
Mohsen Vazirizade <s.m.vazirizade@gmail.com>
2018-09-26 09:53:47 -0700

    Revised smvazirizade,1
    clc has been added.
    rng(1) has been added.
    loading command has been corrected: male100=load('olympic100m.txt')
    t = male100(:,2); % Winning times  
    Improving the figure by adding some comments including legend()

--- matlab/chapter2/genolymp.m
+++ matlab/chapter2/genolymp.m
@@ -1,13 +1,17 @@
 %% genolymp.m
 % From A First Course in Machine Learning, Chapter 2.
 % Simon Rogers, 01/11/11 [simon.rogers@glasgow.ac.uk]
+% Revised by Sayyed Mohsen Vazirizade, Oct/10/2017 [smvazirizade@email.arizona.edu]
 % Generative model for the Olympic data
-clear all;close all;
+clc;clear all;close all;
+rng(1);
 %% Define the parameters - from previous exercises (see fitolymp.m)
 w = [36.4165;-0.0133];
 %% Load the Olympic data to get the Olympic years
-load ../data/olympics
+%load ../data/olympics.mat
+male100=load('olympic100m.txt')
 x = male100(:,1); % Olympic years
+t = male100(:,2); % Winning times
 %% Compute the means from the model
 X = [x.^0 x.^1];
 mean_t = X*w;
@@ -22,10 +26,13 @@ noisy_t = mean_t + randn(size(mean_t)).*sqrt(noise_var);
 %% Add these to the plot
 figure(1);hold on
 plot(x,noisy_t,'bo','markersize',10);
+%% Add the actual data as red dots, for comparison
+plot(x,t,'r.','markersize',10)
+h1=findobj(gca,'Color','k')
+h2=findobj(gca,'Color','b')
+h3=findobj(gca,'Color','r')
 % Draw lines between means and noise
 for i = 1:length(x)
     plot([x(i) x(i)],[mean_t(i),noisy_t(i)],'k--');
 end
-
-%% Add the actual data as red dots, for comparison
-plot(x,male100(:,2),'r.','markersize',10)
+legend([h1 h2 h3],{'Mean values','With added noise','Data'})
