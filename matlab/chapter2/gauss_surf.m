62885fc37029904445ed37fca571f103e4b1499e
Mohsen Vazirizade <s.m.vazirizade@gmail.com>
2018-09-26 09:44:16 -0700

    Revised smvazirizade,1

--- matlab/chapter2/gauss_surf.m
+++ matlab/chapter2/gauss_surf.m
@@ -2,7 +2,8 @@
 % From A First Course in Machine Learning, Chapter 2.
 % Simon Rogers, 01/11/11 [simon.rogers@glasgow.ac.uk]
 % Surface and contour plots of a Gaussian
-clear all;close all;
+% Revised by Sayyed Mohsen Vazirizade, Oct/10/2017 [smvazirizade@email.arizona.edu]
+clc;clear all;close all;
 %% The Multi-variate Gaussian pdf is given by:
 % $p(\mathbf{x}|\mu,\Sigma) =
 % \frac{1}{(2\pi)^{D/2}|\Sigma|^{1/2}}\exp\left\{-\frac{1}{2}(\mathbf{x}-\mu)^T\Sigma^{-1}(\mathbf{x}-\mu)\right\}$
@@ -10,12 +11,14 @@ clear all;close all;
 mu = [1;2];
 sigma = [1 0.8;0.8 4];
 %% Define the grid for visualisation
-[X,Y] = meshgrid(-5:0.1:5,-5:0.1:5);
+x=[-5:0.1:5];
+y=[-5:0.1:5];
+[X,Y] = meshgrid(x,y);
 %% Define the constant
 const = (1/sqrt(2*pi))^2;
 const = const./sqrt(det(sigma));
 temp = [X(:)-mu(1) Y(:)-mu(2)];
-pdfv = const*exp(-0.5*diag(temp*inv(sigma)*temp'));
+pdfv = const*exp(-0.5*diag(temp/(sigma)*temp'));
 pdfv = reshape(pdfv,size(X));
 %% Make the plots
 figure(1);hold off
